a3165a69513a653474b4a746a7d49c43447f9955
shivanshu <shivanshunigam16@gmail.com>
2023-01-02 15:10:20 +0530

    Added profile models

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "pkid", "user", "gender", "phone_number", "country", "city"]
+    list_filter = ["city", "gender", "country"]
+    list_display_links = ["id", "user", "pkid"]
+    
+
+admin.site.register(Profile, ProfileAdmin)
